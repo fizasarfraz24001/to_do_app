603c15544f8ca580c1c020c94a0d1265ee79e38a
fizasarfraz24001 <fizasarfraz24001@gmail.com>
2024-10-21 00:34:36 +0500

    to-do-app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,99 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [todoItem, setTodoItem] = useState<string[]>([])
+  const [todoCompItem, setTodoCompItem] = useState<string[]>([])
+  const [inputField, setInputField] = useState<string>("")
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const addTodoItem = () => {
+    const newTodoItem = [...todoItem]
+    newTodoItem.push(inputField)
+    setTodoItem(newTodoItem)
+    setInputField("")
+  }
+
+  const removeTodoItem = (index: number) => {
+    const removeItem = [...todoItem]
+    removeItem.splice(index, 1)
+    setTodoItem(removeItem)
+  }
+
+  const markAsCompleted = (singleItem: string) => {
+    const newCompItem = [...todoCompItem]
+    newCompItem.push(singleItem)
+    setTodoCompItem(newCompItem)
+  }
+
+  console.log("todoCompItem", todoCompItem)
+  return (
+
+    <div className="min-h-screen bg-gray-600 flex items-center justify-center">
+      <div className="bg-white p-10 rounded shadow-md w-full max-w-md">
+        <h1 className="text-2xl font-bold text-center mb-6">Todo App</h1>
+
+        <div className="flex mb-4">
+          <input
+            className="w-full p-2 border rounded-l outline-none focus:ring-2 focus:ring-blue-500"
+            type="text"
+            placeholder="Enter todo item"
+            value={inputField}
+            onChange={(e) => {
+              setInputField(e.target.value)
+            }}
+          />
+          <button
+            className="bg-purple-600 text-white p-2 rounded-r hover:bg-blue-600 transition"
+            onClick={addTodoItem}
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Add
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <ul className="space-y-2">
+          {
+            todoItem.length > 0 ?
+
+              todoItem.map((singleItem, index) => {
+
+                const markComplete = todoCompItem.includes(singleItem)
+                return (
+                  <li key={index}
+                    className=
+                    {(markComplete ? "bg-blue-950 text-white" : "bg-white") + " flex justify-between items-center p-2 rounded"}
+                  >
+                    <span className="flex-1">{singleItem}</span>
+                    {
+                      markComplete ?
+                        null
+                        :
+                        <>
+                          <button
+                            className="text-blue-950 hover:text-blue-800 border-2 p-2"
+                            onClick={() => markAsCompleted(singleItem)}
+                          >
+                            Complete
+                          </button>
+                          <button
+                            className="text-red-500 hover:text-red-700 border-2 p-2"
+                            onClick={() => removeTodoItem(index)}
+                          >
+                            Remove
+                          </button>
+                        </>
+                    }
+
+                  </li>
+                )
+              })
+
+              :
+              <p>No task for today</p>
+          }
+
+
+        </ul>
+      </div>
     </div>
   );
 }
